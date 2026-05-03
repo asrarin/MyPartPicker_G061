31baea6728686090a464c34e03f9fcdeac3b57e9
asrar <asrarinmad2017@gmail.com>
2026-05-04 07:11:11 +0800

    removed create database function from run.py

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
-from flask import Flask, render_template
+from app import create_app
+app = create_app()
 
-app = Flask(__name__)
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
+if __name__ == '__main__':
+    app.run(debug=True)
